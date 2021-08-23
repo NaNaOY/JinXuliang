391d12a0ba85f9ae5e55371b4f7e0611c1aa9cbb
niuhongwei <nhw1013@outlook.com>
2021-08-23 15:01:53 +0800

    java基础

---
+++ java/src/com/company/CaiNiaoJava/IO_Flow/WriteDemo.java
@@ -0,0 +1,10 @@
+package com.company.CaiNiaoJava.IO_Flow;
+
+public class WriteDemo {
+    public static void main(String[] args){
+        int b;
+        b='A';
+        System.out.println(b);
+        System.out.write('\n');
+    }
+}
