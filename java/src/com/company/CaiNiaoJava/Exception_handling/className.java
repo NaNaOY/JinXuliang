7157d3583c2c16e020ab8562c80f6e692a41aea1
niuhongwei <nhw1013@outlook.com>
2021-08-23 15:01:21 +0800

    异常处理

---
+++ java/src/com/company/CaiNiaoJava/Exception_handling/className.java
@@ -0,0 +1,11 @@
+package com.company.CaiNiaoJava.Exception_handling;
+
+import java.rmi.RemoteException;
+
+public class className {
+    public static void main(double amount) throws RemoteException {
+        //Method implementation
+        throw new RemoteException();
+    }
+    //Remainder of class definition
+}
